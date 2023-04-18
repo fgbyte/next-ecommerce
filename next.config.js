fa73a3612d2f565a161bf0faa6dd9e236c415408
fgbyte <fer.garciabayon@gmail.com>
2023-04-18 13:40:40 -0400

    update

--- next.config.js
+++ next.config.js
@@ -5,3 +5,9 @@ module.exports = {
     includePaths: [path.join(__dirname, 'styles')],
   },
 }
+
+// next.config.js
+const withGoogleTagManager = require('next-plugin-google-tag-manager')
+module.exports = withGoogleTagManager({
+  gtmId: 'GTM-PJFKGZH',
+})
